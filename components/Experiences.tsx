ea3cdb435d6670bae1c48e98bd73fe225cba23b6
~nutcase~ <nutcase@pop-os.localdomain>
2022-01-18 14:54:14 +0300

    Experience display component

---
+++ components/Experiences.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { Experience, UserImg, UserInfo } from '../styles/HomeSection.modules';
+
+interface Props {
+    blog: {
+        username: string
+        employmentStatus: string
+        languages: string[],
+        experience: string
+    }
+}
+
+const Experiences: React.FC<Props> = ({ blog }) => {
+    return (
+        <Experience key={blog.username}>
+            <div className='person-info'>
+                <UserImg>
+                    <img src="/user.jpg" />
+                </UserImg>
+                <UserInfo>
+                    <div>
+                        <p className='username'>{blog.username}</p>
+                        <div className='languages'>{blog.languages}</div>
+                    </div>
+                    <hr />
+                </UserInfo>
+            </div>
+            <div className="experience">
+                {blog.experience}
+            </div>
+        </Experience>
+    )
+}
+
+export default Experiences;
